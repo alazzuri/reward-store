6b60598cfa8959cc75dcf3d73636bf18685fb7eb
Alexis Lazzuri <56130546+alazzuri@users.noreply.github.com>
2020-09-26 14:29:50 -0300

    Adding dropdown menu

---
+++ src/components/DropdownButton/index.tsx
@@ -0,0 +1,64 @@
+//REACT
+import React, { useState } from "react";
+
+//ROUTER
+import { Link } from "react-router-dom";
+
+//ASSETS
+import { ReactComponent as ChevronDown } from "../../assets/icons/chevron-down.svg";
+import { ReactComponent as Coin } from "../../assets/icons/coin.svg";
+
+const DropDownMenu = () => (
+  <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg">
+    <div className="rounded-md bg-white shadow-xs">
+      <div
+        className="py-1"
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="options-menu"
+      >
+        <Link
+          to="/"
+          className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+        >
+          Get More Points
+        </Link>
+        <Link
+          to="/"
+          className="block w-full text-left px-4 py-2 text-sm leading-5 text-gray-900 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+        >
+          Sign out
+        </Link>
+      </div>
+    </div>
+  </div>
+);
+
+const DropDownButton: React.FC<{ points: number }> = ({ points }) => {
+  const [showDropDown, setShowDropDown] = useState<boolean>(false);
+
+  const toggle = () => {
+    setShowDropDown((prevState) => !prevState);
+  };
+
+  return (
+    <div className="relative inline-block text-left">
+      <div>
+        <span className="rounded-md shadow-sm">
+          <button
+            type="button"
+            className="inline-flex justify-center items-center w-full rounded-full py-2 px-4 border border-gray-300 bg-gray-200 leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150"
+            onClick={toggle}
+          >
+            <Coin />
+            <span>{points}</span>
+            <ChevronDown />
+          </button>
+        </span>
+      </div>
+      {showDropDown && <DropDownMenu />}
+    </div>
+  );
+};
+
+export default DropDownButton;
