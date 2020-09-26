0378c566e2074fb648faa7374f881105287828a6
Alexis Lazzuri <56130546+alazzuri@users.noreply.github.com>
2020-09-26 19:30:41 -0300

    Creating pill button component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { ReactComponent as BuyBlue } from "../../assets/icons/buy-blue.svg";
+import { ReactComponent as Coin } from "../../assets/icons/coin.svg";
+
+import PillButton from "../PillButton";
 
 const Card = () => {
   return (
-    <div className="max-w-sm rounded shadow-2xl bg-white divide-y divide-gray-400 h-auto m-2 border-gray-400 px-6">
+    <div className="max-w-sm rounded shadow-2xl bg-white divide-y divide-gray-400 h-auto m-2 border-gray-400 px-6 relative">
       <figure className="px-2 py-4 card-image">
+        <BuyBlue className="absolute right-0 mr-4" />
+        <figcaption className="w-1/2 p-0 absolute right-0 mr-4">
+          <PillButton styles="w-auto bg-gray-900 bg-opacity-50 border-none">
+            <span className="text-sm mr-2 text-white">You need 8000</span>
+            <Coin />
+          </PillButton>
+        </figcaption>
         <img
           className="w-full mx-auto h-full"
           src="https://picsum.photos/100"
---
+++ src/components/PillButton/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const PillButton: React.FC<{ onHandleClick?: () => void; styles?: string }> = ({
+  children,
+  onHandleClick = () => {},
+  styles = "",
+}) => {
+  return (
+    <span className="rounded-md shadow-sm w-full">
+      <button
+        type="button"
+        className={`inline-flex justify-center items-center w-full rounded-full py-2 px-4 border border-gray-300 bg-gray-200 leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150 ${styles}`}
+        onClick={onHandleClick}
+      >
+        {children}
+      </button>
+    </span>
+  );
+};
+
+export default PillButton;
