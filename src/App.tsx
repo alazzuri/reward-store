8bd572b820c4a8e1f6fad5a0a7f255a3d9603db0
Alexis Lazzuri <56130546+alazzuri@users.noreply.github.com>
2020-09-26 18:18:04 -0300

    Template App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Header from "./components/Header";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./tailwind.output.css";
 
-const App = () => <div></div>;
+const App = () => (
+  <div>
+    <Router>
+      <Header />
+    </Router>
+  </div>
+);
 export default App;
