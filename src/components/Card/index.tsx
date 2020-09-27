2f6400ffadbbe480cba898eb5e68af1e28dabc58
Alexis Lazzuri <56130546+alazzuri@users.noreply.github.com>
2020-09-27 13:46:29 -0300

    Creating card views

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,30 +1,117 @@
-import React from "react";
+//REACT
+import React, { useState } from "react";
+
+//COMPONENTS
+import PillButton from "../PillButton";
+
+//ASSETS
+import { ReactComponent as BuyWhite } from "../../assets/icons/buy-white.svg";
 import { ReactComponent as BuyBlue } from "../../assets/icons/buy-blue.svg";
 import { ReactComponent as Coin } from "../../assets/icons/coin.svg";
 
-import PillButton from "../PillButton";
+const RedeemView: React.FC<{
+  show: boolean;
+  requiredPoints: number;
+  buttonTitle?: string;
+  onHandleClick: () => void;
+}> = ({ show, requiredPoints, buttonTitle = "Redeem Now", onHandleClick }) => (
+  <div className={show ? "block" : "hidden"}>
+    <BuyWhite className="absolute right-0 mr-3 top-0 mt-3 z-50" />
+    <figure className="w-full h-full absolute bg-lightblue top-0 left-0 flex justify-center items-center flex-col">
+      <figcaption className="w-1/2 text-white text-3xl flex justify-center items-center h-auto">
+        <span className="h-20 flex items-center">
+          {requiredPoints.toLocaleString()}
+        </span>
+        <Coin className="ml-2 h-full flex items-center mt-2" />
+      </figcaption>
+      <div className="w-3/4 opacity-1">
+        <PillButton
+          styles="py-3 font-bold text-lg bg-white"
+          onHandleClick={onHandleClick}
+        >
+          {buttonTitle}
+        </PillButton>
+      </div>
+    </figure>
+  </div>
+);
 
-const Card = () => {
-  return (
-    <div className="max-w-sm rounded shadow-2xl bg-white divide-y divide-gray-400 h-auto m-2 border-gray-400 px-6 relative">
-      <figure className="px-2 py-4 card-image">
-        <BuyBlue className="absolute right-0 mr-4" />
-        <figcaption className="w-1/2 p-0 absolute right-0 mr-4">
+const DefaultView: React.FC<{
+  name: string;
+  category: string;
+  remainingPoints?: number;
+  imgSrc: string;
+}> = ({ name, category, remainingPoints, imgSrc }) => (
+  <div>
+    {!remainingPoints && (
+      <BuyBlue className="absolute right-0 mr-4 top-0 mt-3" />
+    )}
+    <figure className="px-2 py-4">
+      <figcaption className="w-1/2 p-0 absolute right-0 mr-4">
+        {remainingPoints && (
           <PillButton styles="w-auto bg-gray-900 bg-opacity-50 border-none">
-            <span className="text-sm mr-2 text-white">You need 8000</span>
+            <span className="text-sm mr-2 text-white">
+              {`You need ${remainingPoints}`}
+            </span>
             <Coin />
           </PillButton>
-        </figcaption>
-        <img
-          className="w-full mx-auto h-full"
-          src="https://picsum.photos/100"
-          alt="Sunset in the mountains"
+        )}
+      </figcaption>
+      <img
+        className="w-full mx-auto h-64"
+        src={imgSrc}
+        alt={`product-${name}`}
+      />
+    </figure>
+    <div className="py-4">
+      <div className="font-bold text-base mb-2 text-gray-500">{category}</div>
+      <p className="text-gray-600 text-xl font-bold">{name}</p>
+    </div>
+  </div>
+);
+
+const Card: React.FC<{
+  name: string;
+  category: string;
+  imgSrc: string;
+  requiredPoints: number;
+  remainingPoints?: number;
+  onHandleClick: () => void;
+}> = ({
+  name,
+  category,
+  imgSrc,
+  requiredPoints,
+  remainingPoints,
+  onHandleClick,
+}) => {
+  const [isHover, setIsHover] = useState<boolean>(false);
+
+  const toggleView = () => {
+    setIsHover((prevState) => !prevState);
+  };
+
+  return (
+    <div
+      className={`max-w-sm rounded shadow-2xl bg-white divide-y divide-gray-400 h-auto m-2 border-gray-400 px-6 relative cursor-pointer transition-all duration-75 ${
+        isHover && "transform -translate-y-5 box-shadow-hover"
+      }`}
+      onMouseEnter={toggleView}
+      onMouseLeave={toggleView}
+    >
+      <DefaultView
+        name={name}
+        category={category}
+        imgSrc={imgSrc}
+        remainingPoints={remainingPoints}
+      />
+      {!remainingPoints && (
+        <RedeemView
+          show={isHover}
+          requiredPoints={requiredPoints}
+          onHandleClick={onHandleClick}
         />
-      </figure>
-      <div className="py-4">
-        <div className="font-bold text-base mb-2 text-gray-500">Phones</div>
-        <p className="text-gray-600 text-xl font-bold">Iphone 8</p>
-      </div>
+      )}
     </div>
   );
 };
