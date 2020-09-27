dfdb84165027e9dea48a5a8fb3196e966c2c26dc
Alexis Lazzuri <56130546+alazzuri@users.noreply.github.com>
2020-09-27 14:49:28 -0300

    Testing components in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+//REACT
 import React from "react";
 
-function App() {
-  return <div className="App"></div>;
-}
+//COMPONENTS
+import Header from "./components/Header";
 
+//ROUTER
+import { BrowserRouter as Router } from "react-router-dom";
+
+//TAILWIND
+import "./tailwind.output.css";
+import Card from "./components/Card";
+
+const App = () => (
+  <div className="bg-gray-100">
+    <Router>
+      <Header />
+      <Card
+        name="Kodak 234"
+        category="Cameras"
+        requiredPoints={10000}
+        onHandleClick={() => alert("lala")}
+        imgSrc="https://picsum.photos/200"
+      />
+    </Router>
+  </div>
+);
 export default App;
