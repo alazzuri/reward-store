f2c866c163dc60012e720b67d86036be477938fc
Alexis Lazzuri <56130546+alazzuri@users.noreply.github.com>
2020-10-11 12:46:34 -0300

    Extracting banner from header

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,10 @@ import "./tailwind.output.css";
 import Spinner from "./components/Spinner";
 import HistoryItem from "./components/HistoryItem";
 import HistoryContainer from "./containers/HistoryContainer";
+import HistoryPage from "./pages/HistoryPage";
+import Banner from "./components/Banner";
+import MainImage from "./assets/images/header-x1.png";
+import MainImage2x from "./assets/images/header-x2.png";
 
 const App = () => (
   <div className="bg-gray-100 w-full">
@@ -27,10 +31,16 @@ const App = () => (
       <Router>
         <Suspense fallback={<Spinner />}>
           <Header />
-          <FilterBar />
+          {/* <FilterBar />
           <ProductsContainer />
           <HistoryContainer />
-          <Pagination />
+          <Pagination /> */}
+          <Banner
+            title="Electronics"
+            imgSrc={MainImage}
+            srcSet={`${MainImage2x} 2x`}
+          />
+          <HistoryPage></HistoryPage>
           <Footer />
         </Suspense>
       </Router>
