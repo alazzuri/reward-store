a855eb6c11845f9d83bc1219c5f7adeb4503b882
Alexis Lazzuri <56130546+alazzuri@users.noreply.github.com>
2020-10-31 19:20:07 -0300

    Fixing HistoryItem Test

--- src/__test__/mock/history.ts
+++ src/__test__/mock/history.ts
@@ -2,6 +2,6 @@ export const mockedHistoryItem = {
   name: "Kodak 234",
   category: "Cameras",
   cost: 10000,
-  imgSrc: " https://picsum.photos/200",
-  date: "23/05/2020",
+  img: { url: "https://picsum.photos/200", hdUrl: "https://picsum.photos/200" },
+  date: "05-23-2020",
 };
