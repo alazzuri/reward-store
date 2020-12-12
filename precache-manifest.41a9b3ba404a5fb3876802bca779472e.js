4333a26890b3d5de1c875eef3a9ef4912cabee65
Alexis Lazzuri <56130546+alazzuri@users.noreply.github.com>
2020-12-12 19:21:13 -0300

    Updates

---
+++ precache-manifest.41a9b3ba404a5fb3876802bca779472e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e96ad632610c1e7bad9b3ebbfc57c0",
+    "url": "/reward-store/index.html"
+  },
+  {
+    "revision": "5db416c9aaffb6b7d9fd",
+    "url": "/reward-store/static/css/2.adb6ee89.chunk.css"
+  },
+  {
+    "revision": "1d702849cbc47988d1f6",
+    "url": "/reward-store/static/css/main.02abc8d8.chunk.css"
+  },
+  {
+    "revision": "5db416c9aaffb6b7d9fd",
+    "url": "/reward-store/static/js/2.b47899d9.chunk.js"
+  },
+  {
+    "revision": "0232e82b05983d312a32c81ef442b650",
+    "url": "/reward-store/static/js/2.b47899d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d702849cbc47988d1f6",
+    "url": "/reward-store/static/js/main.2744e573.chunk.js"
+  },
+  {
+    "revision": "94597d0da7339db9b02b",
+    "url": "/reward-store/static/js/runtime-main.deb683d4.js"
+  },
+  {
+    "revision": "b702be85581f5bb0ad78305bc01f7acf",
+    "url": "/reward-store/static/media/404.b702be85.png"
+  },
+  {
+    "revision": "8afe435db802dd58cf27361f4c1152a4",
+    "url": "/reward-store/static/media/Error.8afe435d.png"
+  },
+  {
+    "revision": "2e6efe58997cf9953ce94c4829d49f8e",
+    "url": "/reward-store/static/media/arrow-left.2e6efe58.svg"
+  },
+  {
+    "revision": "bedef3832219792f30be7f676dffa61a",
+    "url": "/reward-store/static/media/arrow-right.bedef383.svg"
+  },
+  {
+    "revision": "2f11a957b72ff4a5c46227027cf46115",
+    "url": "/reward-store/static/media/buy-blue.2f11a957.svg"
+  },
+  {
+    "revision": "40bc281346af2ba434a9d9792491715d",
+    "url": "/reward-store/static/media/buy-white.40bc2813.svg"
+  },
+  {
+    "revision": "45b4f9cd3046434eab0736ef27114be8",
+    "url": "/reward-store/static/media/chevron-down.45b4f9cd.svg"
+  },
+  {
+    "revision": "4669120cae147258c9fc868719b1d3ca",
+    "url": "/reward-store/static/media/coin.4669120c.svg"
+  },
+  {
+    "revision": "f0b98264ce8c93768fbf37c263885aaa",
+    "url": "/reward-store/static/media/header-x1.f0b98264.png"
+  },
+  {
+    "revision": "a343e15346be343af7867fa8f95ad9d5",
+    "url": "/reward-store/static/media/header-x2.a343e153.png"
+  },
+  {
+    "revision": "97a787bda31bac88ec3f6f765aaf9b1f",
+    "url": "/reward-store/static/media/main-logo.97a787bd.svg"
+  }
+]);
